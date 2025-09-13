7d0aa0cc566c6f899aa6f1dc864206f57b257681
DrDaroo <cegladaroo@gmail.com>
2025-09-13 19:36:31 +0200

    chore: changed site icon and description

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export const metadata = {
   title: "Przebieczany",
-  description: "Przebieczany",
-  image: "/favicon.ico",
+  description: "Nowoczesne domy od dewelopera w Przebieczanach - spokojna okolica, komfort życia i świetna lokalizacja niedaleko Krakowa.",
+  image: "/przebieczany_logo.svg",
 };
 
 export default function Head() {
